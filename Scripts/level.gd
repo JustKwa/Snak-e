6b38edcd9df3d81734e5018f89a543689fe44df0
Khoa Nguyen <s3891524@rmit.edu.vn>
2022-10-25 20:49:40 +0700

    Better Death animation with retexture for the food

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 const GLOBAL_VAR: Resource = preload("res://global_var.tres")
 
-export var speed: int
+export var speed: float
 
 onready var food = preload("res://Scenes/food.tscn")
 
-var old_score: int = 1
+var old_score: int = 0
 
 
 func _ready():
@@ -22,14 +22,14 @@ func _process(_delta):
 func difficulty_check():
 	if score.player_score - old_score >= 10:
 		old_score = score.player_score
-		GLOBAL_VAR.speed += GLOBAL_VAR.speed * 0.1
+		GLOBAL_VAR.speed += GLOBAL_VAR.speed * 0.05
 
 
 func spawn_food():
 	var instance = food.instance()
 	var x = rand_range(1, 11)
 	var y = rand_range(1, 11)
-	instance.position = $grid.map_to_world(Vector2(x, y)) + Vector2(8, 8)
+	instance.position = $grid.map_to_world(Vector2(x, y)) + Vector2(9, 9)
 	instance.connect("food_eaten", self, "is_eaten")
 	call_deferred("add_child", instance)
 
@@ -40,4 +40,5 @@ func is_eaten():
 
 
 func _on_snake_game_over():
-	return get_tree().reload_current_scene()
+	$snake.get_node('head').animation_player.play('death')
+	GLOBAL_VAR.speed = 0
