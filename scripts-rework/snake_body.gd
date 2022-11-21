0c8412cdca087eff36c5c1f93f8a99513aba46f0
Khoa Nguyen <justkwaofficial@gmail.com>
2022-11-21 14:06:21 +0700

    Optimize the codes of snake objects

---
+++ scripts-rework/snake_body.gd
@@ -0,0 +1,29 @@
+extends SnakeBody
+
+var disconnect: bool = false
+
+signal game_over
+
+
+func bounce():
+	animation_player.play("bounce")
+
+
+func change_dir() -> void:
+	self.position = Vector2(round(self.position.x), round(self.position.y))
+	current_pos = self.position
+	percent_to_tile = 0.0
+	direction *= -1
+
+
+func _on_body_area_entered(area: Area2D) -> void:
+	if "body" in area.name:
+		queue_free()
+		score.player_score += 1
+	elif "head" in area.name && disconnect:
+		emit_signal("game_over")
+
+
+func _on_body_area_exited(area: Area2D) -> void:
+	if !disconnect && "head" in area.name:
+		disconnect = true
---
+++ scripts-rework/snake_class.gd
@@ -0,0 +1,30 @@
+class_name SnakeBody extends Area2D
+
+const GLOBAL_VAR: Resource = preload("res://global_var.tres")
+const GRID_SIZE = global_var.GRID_SIZE
+
+var direction
+var current_pos = self.position
+var percent_to_tile = 0.0
+
+onready var animation_player = get_node("AnimationPlayer")
+
+
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+	_move(delta)
+
+
+func _move(delta):
+	percent_to_tile += (GLOBAL_VAR.speed * 0.8) * delta
+
+	if percent_to_tile >= 1.0:
+		position = current_pos + (direction * GRID_SIZE)
+		current_pos = self.position
+		percent_to_tile = 0.0
+
+	else:
+		position = current_pos + (GRID_SIZE * percent_to_tile * direction)
