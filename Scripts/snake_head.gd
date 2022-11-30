011de40f20fda7ac0dba5fdebb30f3c1be583b42
Khoa Nguyen <justkwaofficial@gmail.com>
2022-11-30 12:46:39 +0700

    New shoot mechanic

--- Scripts/snake_head.gd
+++ Scripts/snake_head.gd
@@ -8,6 +8,7 @@ enum State { ROTATE, MOVE, SHOOT }
 var rotation_angle: int = RotateAngle.RIGHT
 
 onready var snake_controller = get_parent()
+onready var animation_player2 = $AnimationPlayer2
 
 
 func _ready():
@@ -24,13 +25,20 @@ func _physics_process(delta):
 			_move(delta)
 			if _is_rotate():
 				state = State.ROTATE
+			elif _is_shoot():
+				state = State.SHOOT
 		State.SHOOT:
 			_shoot()
 			state = State.MOVE
 
 
 func on_food_eaten() -> void:
-	animation_player.play("eat_hold")
+	if "hold_food" in animation_player2.current_animation:
+		state = State.SHOOT
+		yield(animation_player2, "animation_finished")
+		animation_player2.play("hold_food")
+	else:
+		animation_player.play("eat_hold")
 
 
 func _move(delta):
@@ -68,7 +76,16 @@ func _rotate(value: int):
 	set_rotation_degrees(value)
 
 
+func _is_shoot() -> bool:
+	if (
+		Input.is_action_just_pressed("ui_shoot")
+		&& "hold_food" in animation_player2.current_animation
+	):
+		return true
+	else:
+		return false
+
+
 func _shoot():
-	if Input.is_action_just_pressed("ui_shoot"):
-		animation_player.play("eat_shoot")
-		self.modulate = 0
+	animation_player.play("eat_shoot")
+	animation_player2.play("neutral")
