1de1ada582033aa7d9fb43ede11020926c3b2eba
Khoa Nguyen <s3891524@rmit.edu.vn>
2022-12-13 09:29:29 +0700

    Delete grid global var

--- Scripts/item.gd
+++ Scripts/item.gd
@@ -7,5 +7,4 @@ onready var spawn_location = self.position
 
 
 func _ready():
-	var grid_position = Grid.world_to_map(spawn_location)
-	emit_signal("spawned", grid_position)
+	emit_signal("spawned", spawn_location)
