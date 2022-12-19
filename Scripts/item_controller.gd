33cd9dd74016d5b49faa4e59c3e78e6edf5d3f5e
Khoa Nguyen <justkwaofficial@gmail.com>
2022-12-19 20:03:07 +0700

    Add Food into item controller

--- Scripts/item_controller.gd
+++ Scripts/item_controller.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 signal cells_occupied(occupied_cells)
+signal food_eaten
 
 var grid_cells
-var occupied_cells : PoolVector2Array
+var occupied_cells: PoolVector2Array
 
 onready var obstacle = preload("res://Scenes/obstacle.tscn")
+onready var food = preload("res://Scenes/food.tscn")
 onready var grid = get_parent().get_node("grid")
 onready var level = get_parent()
 
@@ -14,6 +16,7 @@ func _ready():
 	level.connect("spawn_obstacle", self, "_on_spawn_obstacle")
 	grid.connect("cells_available", self, "_on_cells_available")
 	grid_cells = grid.get_used_cells()
+	_on_spawn_food()
 
 
 func _on_cells_available(available_cells):
@@ -32,3 +35,17 @@ func _on_spawned(grid_position):
 	var spawn_location = grid.world_to_map(grid_position)
 	occupied_cells.append(spawn_location)
 	emit_signal("cells_occupied", occupied_cells)
+
+
+func _on_spawn_food():
+	var instance = food.instance()
+	var rand_index = randi() % grid_cells.size()
+	instance.position = grid.map_to_world(grid_cells[rand_index]) + Vector2(8, 9)
+	instance.connect("spawned", self, "_on_spawned")
+	instance.connect("food_eaten", self, "_on_eaten")
+	call_deferred("add_child", instance)
+
+
+func _on_eaten():
+	_on_spawn_food()
+	emit_signal("food_eaten")
